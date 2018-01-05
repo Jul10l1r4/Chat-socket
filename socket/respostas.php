894b24761272ccf89ea36661158705cd19140d7f
Julio Lira <32502168+Jul10l1r4@users.noreply.github.com>
2018-01-05 09:06:40 -0300

    Update respostas.php

--- socket/respostas.php
+++ socket/respostas.php
@@ -41,16 +41,14 @@ while (true) {
 	foreach ($changed as $changed_socket) {	
 		
 		//check for any incomming data
-		while(socket_recv($changed_socket, $buf, 1024, 0) >= 1)
-		{
+		while(socket_recv($changed_socket, $buf, 1024, 0) >= 1)	{
 			$received_text = unmask($buf); //unmask data
 			$tst_msg = json_decode($received_text); //json decode 
 			$user_name = $tst_msg->name; //sender name
 			$user_message = $tst_msg->message; //message text
-			$user_color = $tst_msg->color; //color
 			
 			//prepare data to be sent to client
-			$response_text = mask(json_encode(array('type'=>'usermsg', 'name'=>$user_name, 'message'=>$user_message, 'color'=>$user_color)));
+			$response_text = mask(json_encode(array('type'=>'usermsg', 'name'=>$user_name, 'message'=>$user_message)));
 			send_message($response_text); //send data
 			break 2; //exist this loop
 		}
