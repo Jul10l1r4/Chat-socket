ea7a9a31d4d436826802d5a70971d6b7b1fddb5e
Julio Lira <32502168+Jul10l1r4@users.noreply.github.com>
2018-01-21 22:32:46 -0300

    Abra-me e edite-me, antes de usar

--- disparar.php
+++ disparar.php
@@ -1,7 +1,9 @@
     <?php
     //          ↓↓↓↓↓↓↓↓ Esse é o caminho do php  ↓↓←←← E este é o que eu desejo executar
-    		exec("/usr/bin/php /var/www/html/chat/msg.php");
+    		exec("/usr/bin/php /var/www/html/Chat-Socket-PHP/socket/respostas.php");
     // Ele irá disparar o arquivo, executará tal código no terminal 
    
-   //atenção, uma vez que executado precisa de cautela em saber qual processo desejas fechar, para o servidor
-    //, precisa ser disparado apenas uma vez
+   // atenção, uma vez que executado precisa
+   // de cautela em saber qual processo desejas
+   // fechar, para o servidor, precisa ser
+   // disparado apenas uma vez
